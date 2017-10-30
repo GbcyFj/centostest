48c51f22b30958101f13848ad541fc1e27c1ef3a
Eric Martinez <emartinez@usgs.gov>
2017-10-30 16:45:18 -0600

    Update ci script.

--- ci-jenkins.groovy
+++ ci-jenkins.groovy
@@ -2,7 +2,8 @@ node {
   stage('build') {
     sh '''
       pwd
-      ls
+      ls -la
+      echo ${IMAGE_TAG_VERSION}
       docker --version
       docker container ls -a
       docker image ls
