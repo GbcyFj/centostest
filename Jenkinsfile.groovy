9bbbebcd22c726615a2492553c59c63cfe81e26d
Eric Martinez <emartinez@usgs.gov>
2018-03-13 11:15:14 -0600

    Trying to push in a different way.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -55,12 +55,25 @@ node {
         }
       }
 
-      docker.withRegistry(
-        "https://registry.hub.docker.com",
-        'dockerhub-usgshazdevcicd'
-      ) {
+      withCredentials([usernamePassword(
+        credentialsId: 'dockerhub-usgshazdevcicd',
+        passwordVariable: 'HUB_USERNAME',
+        usernameVariable: 'HUB_PASSWORD'
+      )]) {
+
+      // docker.withRegistry(
+      //   "https://registry.hub.docker.com",
+      //   'dockerhub-usgshazdevcicd'
+      // ) {
         ansiColor('xterm') {
-          sh "docker push usgs/centos:${IMAGE_VERSION}"
+          sh """
+            docker login \
+              https://registry.hub.docker.com \
+              --username ${HUB_USERNAME} \
+              --password ${HUB_PASSWORD}
+            docker push usgs/centos:${IMAGE_VERSION}
+          """
+          // sh "docker push usgs/centos:${IMAGE_VERSION}"
         }
       }
     }
